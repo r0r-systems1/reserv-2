785326db94d48d1a18f4d26cd3c3f6847da88535
Arelem <arelem3@bigpond.com>
2013-03-16 14:25:12 +1100

    Added User and admin controllers -mar16

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130311061159) do
+ActiveRecord::Schema.define(:version => 20130316010137) do
 
   create_table "attendee_eas_types", :force => true do |t|
     t.integer  "eas_types_id", :null => false
@@ -94,6 +94,14 @@ ActiveRecord::Schema.define(:version => 20130311061159) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "initiate"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hashed_password"
+    t.string   "salt"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
